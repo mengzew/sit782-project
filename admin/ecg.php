33bb972c9cbdd7f7340123f344bef2e39538ab1d
rshresth3918 <38995074+rshresth3918@users.noreply.github.com>
2018-05-17 22:02:59 +1000

    Update ecg.php
    Updated codes.

--- admin/ecg.php
+++ admin/ecg.php
@@ -141,6 +141,7 @@ session_start();
         <div class="row">
             <div class="col-md-12">
                 <div class="col-md-4 table-bordered ecg-table">
+                <h4>Patient Data</h4>
                 <table class="table">
                 <thead>
                     <tr>
@@ -149,7 +150,7 @@ session_start();
                     </tr>
                 </thead>    
                     <tr>
-                        <td>Dose</td>
+                        <td><a class="trigger" href="dose.php">Dose</a></td>
                         <td>100</td>
                     </tr>
                     <tr>
@@ -206,12 +207,12 @@ session_start();
             <div class="col-md-6">
             
                  <h4>COMMENTS</h4>
-                 <p>05:07 Hi NAME, you’ve warmed up well this morning, and your heart
-                  and muscles will be primed to increase the intensity level. We’re aiming
-                  to stay at 50 to 55% HRR for the next 40 minutes, so let’s increase your
+                 <p>05:07 Hi NAME, youâ€™ve warmed up well this morning, and your heart
+                  and muscles will be primed to increase the intensity level. Weâ€™re aiming
+                  to stay at 50 to 55% HRR for the next 40 minutes, so letâ€™s increase your
                    speed a little bit more. Great work, go for it!</p>
                    
-                 <p>Hi NAME, you’re working at 53% HRR so…</p>  
+                 <p>Hi NAME, youâ€™re working at 53% HRR soâ€¦</p>  
             
             
             </div>
